fe3904d6a55db87ae63c097ee7cfc927aae00b59
Tomoko Suzuki <suzuki_tomoko@cyberagent.co.jp>
2014-07-15 17:01:57 +0900

    コメント欄の微修正

--- commentform/app/scripts/main.js
+++ commentform/app/scripts/main.js
@@ -3,23 +3,24 @@
  */
 var app =  angular.module('comment', []);
 app.controller('commentController', ['$scope', function($scope) {
-$scope.master = {};
-$scope.commentList = [];
+  $scope.commentList = [];
 
-$scope.add = function() {
-  $scope.commentList.push($scope.comment);
-  $scope.comment = "";
-  console.log($scope.commentList);
-};
-
-$scope.reset = function() {
-  if(window.confirm('内容をリセットしていいですか？')) {
+  $scope.add = function() {
+    var date = new Date();
+    addTime = date.getTime();
+    // comment = $scope.comment.replace(/\n/g, '<br/>');
+    $scope.commentList.push({'comment': $scope.comment, 'addTime': addTime});
     $scope.comment = "";
-  }
-};
+    console.log($scope.commentList);
+  };
 
-$scope.delete = function($index) {
-  $scope.commentList.splice($index, 1);
-};
+  $scope.reset = function() {
+    if(window.confirm('内容をリセットしていいですか？')) {
+      $scope.comment = "";
+    }
+  };
 
+  $scope.delete = function($index) {
+    $scope.commentList.splice($index, 1);
+  };
 }]);
